75301ee2ab27d914200e025ebf5a3960e6586171
S-H-Y-GitHub <932495000@qq.com>
2017-03-27 19:28:32 +0800

    reformat code and test push

--- src/lexicalAnalyzer/Main.java
+++ src/lexicalAnalyzer/Main.java
@@ -33,7 +33,7 @@ public class Main
 					//处理空白字符
 					if (buffer.substring(i, i + 1).matches("\\s"))
 						i++;
-						//处理单个的符号
+					//处理单个的符号
 					else if (buffer.substring(i, i + 1).matches("#|;|\\)|\\(|\\{|}|\\.|\\[|]|:|\\?"))
 					{
 						System.out.println("(\"" + buffer.substring(i, i + 1) + "\"\t" + buffer.substring(i, i + 1) + ")");
