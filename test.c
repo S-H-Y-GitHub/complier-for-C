487be90f449d70f4e53e02995f40fbd679d2668f
S-H-Y-GitHub <932495000@qq.com>
2017-04-24 14:41:56 +0800

    完成文法产生式

--- test.c
+++ test.c
@@ -1,10 +1,14 @@
 int main()
 {
-	char a = '0';
+	char a;
 	int[13] b;
+	int i;
 	b = {0};
-	for(int i = 0;i < 13; i++)
-		b[i] += a;
-	printf("hello world");
+	for(i = 0;i < 13; i=i+1)
+		i = a + 1;
+	if(i==100)
+	    a = 'c';
+	else
+	    a = 'd';
 	return 0;
 }
